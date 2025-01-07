9fbbb2ab81aa92dd4bafa8a3c284222d4f88c689
lewdguri <gurix.cz@gmail.com>
2025-01-07 23:49:18 +0100

    Add BG, styling, scrollbar

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,52 @@
+const landerDot = document.getElementById('lander-dot');
+const aboutDot = document.getElementById('about-dot');
+const projectsDot = document.getElementById('projects-dot');
+
+
 const scrollToTop = () => {
     window.scrollTo({top: 0, behavior: 'smooth'});
 }
+
+const scrollToSection = (section) => {
+    const sectionElement = document.getElementById(section);
+    if (sectionElement) {
+        sectionElement.scrollIntoView({behavior: 'smooth'});
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    function detectCurrentPage() {
+        const sections = document.querySelectorAll('.page');
+        let currentSection = null;
+
+        sections.forEach(section => {
+            const rect = section.getBoundingClientRect();
+            if (rect.top >= 0 && rect.top < window.innerHeight / 2) {
+                currentSection = section;
+            }
+        });
+
+        if (currentSection) {
+            switch (currentSection.id) {
+                case 'welcome':
+                    landerDot.classList.add('active');
+                    aboutDot.classList.remove('active');
+                    projectsDot.classList.remove('active');
+                    break;
+                case 'about':
+                    landerDot.classList.remove('active');
+                    aboutDot.classList.add('active');
+                    projectsDot.classList.remove('active');
+                    break;
+                case 'projects':
+                    landerDot.classList.remove('active');
+                    aboutDot.classList.remove('active');
+                    projectsDot.classList.add('active');
+                    break;
+            }
+        }
+    }
+
+    window.addEventListener("scroll", detectCurrentPage);
+    detectCurrentPage(); // Initial check on page load
+});
