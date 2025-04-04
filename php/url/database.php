d40b74f74be8a31e0d86c8e46eb80f731f70b39c
lewdguri <gurix.cz@gmail.com>
2025-04-04 12:47:17 +0200

    add url shortener

---
+++ php/url/database.php
@@ -0,0 +1,63 @@
+<?php
+class Database {
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+
+    private $conn;
+
+    public function __construct() {
+        $this->host = $_ENV['DB_HOST'];
+        $this->db_name = $_ENV['DB_NAME'];
+        $this->username = $_ENV['DB_USER'];
+        $this->password = $_ENV['DB_PASS'];
+    }
+
+    public function connect()
+    {
+        $this->conn = null;
+
+        try {
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+            if ($this->conn->connect_error) {
+                die("Connection failed: " . $this->conn->connect_error);
+            }
+        } catch (Exception $e) {
+            echo 'Connection Error: ' . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+
+    public function close() {
+        $this->conn->close();
+    }
+
+    public function getUrl($id) {
+        $stmt = $this->conn->prepare("SELECT target FROM url_shortener WHERE id = ?");
+        $stmt->bind_param("s", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            return $row['target'];
+        } else {
+            return null;
+        }
+    }
+
+    public function shortenUrl($url) {
+        $unique_id = uniqid();
+        $stmt = $this->conn->prepare("INSERT INTO urls (id, target) VALUES (?, ?)");
+        $stmt->bind_param("ss", $unique_id, $url);
+        if($stmt->execute() === TRUE) {
+            return $unique_id;
+        } else {
+            return null;
+        }
+    }
+
+}
+?>
---
+++ php/url/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>URL Shortener</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>URL Shortener</h1>
+        <form action="index.php" method="post" autocomplete="off">
+            <input class="input-url" type="text" name="url" placeholder="Enter URL" required>
+            <input class="button-submit" type="submit" value="Shorten">
+        </form>
+    </div>
+</body>
+</html>
+
+
+<?php
+
+include 'database.php';
+
+$db = new Database();
+$conn = $db->connect();
+
+if(isset($_GET['url'])) {
+    $url = $db->getUrl($_GET['url']);
+
+    if($url !== null) {
+        header('Location: ' . $url);
+        exit;
+    } else {
+        echo 'URL not found';
+    }
+    $stmt->close();
+}
+
+if(isset($_POST['url'])) {
+    $url = $_POST['url'];
+    $url = filter_var($url, FILTER_SANITIZE_URL);
+
+    if(filter_var($url, FILTER_VALIDATE_URL)) {
+        $unique_id = $db->shortenUrl($url);
+    
+        if($unique_id !== null) {
+            echo 'URL was shortened to ' . "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]/?url=$unique_id";
+        } else {
+            echo 'Error: ' . $conn->error;
+        }
+    } else {
+        echo 'Invalid URL';
+        exit;
+    }
+}
+?>
